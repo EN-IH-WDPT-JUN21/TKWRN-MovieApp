caa5f562a5285a836c45f3b869728b95cc70013f
Anna Nodzyńska <7143565+neternefer@users.noreply.github.com>
2021-11-08 20:59:31 +0100

    Revert "Add playlist-service inner structure"

--- playlist-service/src/main/java/com/ironhack/playlistservice/repository/PlaylistRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ironhack.playlistservice.repository;
-
-import com.ironhack.playlistservice.dao.Playlist;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PlaylistRepository extends JpaRepository<Playlist, Long> {
-}
