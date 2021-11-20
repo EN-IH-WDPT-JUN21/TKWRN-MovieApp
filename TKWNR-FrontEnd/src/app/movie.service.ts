5a228e4c70801e656b55e7e7ad29489f141096d3
Omakatayo <81194365+Omakatayo@users.noreply.github.com>
2021-11-20 14:11:37 +0100

    Before

---
+++ TKWNR-FrontEnd/src/app/movie.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Movie } from './movie';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class MovieService {
+
+  private baseUrl = 'http://localhost:8100/api/movies';
+
+  constructor(private http: HttpClient) { }
+
+  getMovie(id: number): Observable<any> {
+    return this.http.get(`${this.baseUrl}/get/${id}`);
+  }
+
+  getAllMovies(): Observable<Movie[]> {
+    return this.http.get<Movie[]>(`${this.baseUrl}`);
+  }
+
+  getMovieByTitle(title: string): Observable<any> {
+    return this.http.get(`${this.baseUrl}/get?title=title`);
+  }
+
+  saveMovie(movie: Object): Observable<Object> {
+    return this.http.post(`${this.baseUrl}/new`, movie);
+  }
+
+  deleteMovie(id: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/delete/${id}`, { responseType: 'text' });
+  }
+
+}
