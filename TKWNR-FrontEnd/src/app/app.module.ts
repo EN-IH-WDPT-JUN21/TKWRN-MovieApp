824ab0a7fb3ebfc0dfff8f77121dca4d4ad1b37d
ShaunRly <shaundr@hotmail.co.uk>
2021-11-20 12:44:42 +0000

    currently working version

--- TKWNR-FrontEnd/src/app/app.module.ts
+++ TKWNR-FrontEnd/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { MatMenuModule} from '@angular/material/menu';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginFormComponent } from './login-form/login-form.component';
@@ -11,7 +10,6 @@ import { RegisterFormComponent } from './register-form/register-form.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
 import { PlaylistArrayComponent } from './playlist-array/playlist-array.component';
 import { PlaylistDetailComponent } from './playlist-detail/playlist-detail.component';
 import { PlaylistMovieComponent } from './playlist-movie/playlist-movie.component';
@@ -34,7 +32,6 @@ import { PersonDetailsComponent } from './person-details/person-details.componen
     RegisterFormComponent,
     HomeComponent,
     FooterComponent,
-    HeaderComponent,
     PlaylistArrayComponent,
     PlaylistDetailComponent,
     PlaylistMovieComponent,
